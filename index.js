67eca2c81162962e6acf8e8e72020d3fee35c70b
Will Jarvis <will@toohotlimited.com>
2019-03-11 19:52:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyPrependDriver (name){
 function destructivelyRemoveLastDriver (name){
   drivers.pop();
 }
-function destructivelyRemoveLastDriver (name){
+function destructivelyRemoveFirstDriver (name){
   drivers.shift();
 }
 
