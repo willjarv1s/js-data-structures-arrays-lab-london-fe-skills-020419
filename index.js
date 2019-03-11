9fe48ea5d94988cb191fd857e541711489e7c467
Will Jarvis <will@toohotlimited.com>
2019-03-11 19:48:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver (name){
   drivers.push ('Ralph');
 }
-function destructivelyAppendDriver (name){
+function destructivelyPrependDriver (name){
   drivers.push ('Ralph');
 }
